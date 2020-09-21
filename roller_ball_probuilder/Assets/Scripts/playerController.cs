bc4b05f97820a2a41a44a23d437ba78275781901
2fiveboneyard <2fiveboneyard@gmail.com>
2020-09-20 23:14:59 -0600

    add scripts
    added pick up object and text for win condition

--- roller_ball_probuilder/Assets/Scripts/playerController.cs
+++ roller_ball_probuilder/Assets/Scripts/playerController.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    public float speed = 0;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winTextObject.SetActive(false);
     }
 
-    private void OnMove(InputValue movementValue)
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
@@ -23,8 +31,30 @@ public class playerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
-    private void FixedUpdate()
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 23)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+            
+        }
     }
 }
